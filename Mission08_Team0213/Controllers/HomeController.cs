425e74a4341b8b2964a8ae24e47b27ece6d55d2b
carolineconley12 <cconley6@byu.edu>
2024-02-26 19:21:07 -0700

    rough draft on home controllers

--- Mission08_Team0213/Controllers/HomeController.cs
+++ Mission08_Team0213/Controllers/HomeController.cs
@@ -28,5 +28,70 @@ namespace Mission08_Team0213.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        [HttpGet]
+        public IActionResult Form()
+        {
+            ViewBag.Categories = _context.Categories.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form(Movie response)
+        {
+            _context.Movies.Add(response);
+            _context.SaveChanges();
+
+            return View("Confirmation", response);
+        }
+
+        [HttpGet]
+        public IActionResult List()
+        { // Go to table that shows the movie list
+            var applications = _context.Movies
+                .Include("Category")
+                .ToList();
+
+            return View(applications);
+
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var recordToEdit = _context.Movies
+                .Single(x => x.MovieId == id);
+
+            ViewBag.Categories = _context.Categories.ToList();
+
+            return View("Form", recordToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Movie updatedInfo)
+        {
+            _context.Update(updatedInfo);
+            _context.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var recordToDelete = _context.Movies
+                .Single(x => x.MovieId == id);
+
+            return View(recordToDelete);
+
+        }
+        [HttpPost]
+        public IActionResult Delete(Movie movie)
+        {
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
     }
 }
