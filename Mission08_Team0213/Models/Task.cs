962c83d03d4b72c908d181c99a04dc0d25f8230d
mikaylarandall <mikaylamrandall@gmail.com>
2024-02-27 18:36:38 -0700

    Model Stuff

---
+++ Mission08_Team0213/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission08_Team0213.Models
+{
+    public class Task
+    {
+        [Key]
+        [Required]
+        public int TaskId { get; set; }
+        public string TaskName { get; set; }
+        public DateTime? DueDate { get; set; }
+        public int Quadrant { get; set; }
+        public string? Category { get; set; }
+        public bool? Completed { get; set; }
+
+    }
+}
