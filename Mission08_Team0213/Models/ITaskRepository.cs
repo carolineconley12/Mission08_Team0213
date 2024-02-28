87a542e4abcb8010288e1f188d82532d841bdd2c
mikaylarandall <mikaylamrandall@gmail.com>
2024-02-28 08:55:06 -0700

    Repo

--- Mission08_Team0213/Models/ITaskRepository.cs
+++ Mission08_Team0213/Models/ITaskRepository.cs
@@ -4,5 +4,9 @@
     {
         List<TaskTemplate> Tasks { get; }
         public void AddTask(TaskTemplate task);
+
+        public void EditTask(TaskTemplate task);
+
+        public void DeleteTask(TaskTemplate task);
     }
 }
