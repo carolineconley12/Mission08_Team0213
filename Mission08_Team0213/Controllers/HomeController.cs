6431598022ae89bf55e4c5ad4e9789e6411d229a
mikaylarandall <mikaylamrandall@gmail.com>
2024-02-28 17:49:06 -0700

    category

--- Mission08_Team0213/Controllers/HomeController.cs
+++ Mission08_Team0213/Controllers/HomeController.cs
@@ -20,10 +20,9 @@ namespace Mission08_Team0213.Controllers
 
         public IActionResult Index()
         {
-            
+
             var all = _repo.Tasks.Include(x => x.Category)
-                .Where(x => x.Completed == true)
-                .ToList();
+                .Where(x => x.Completed == true);
 
             return View("Index", all);
 		}
